3df49860c117e4fcaa248a188c60a0cbbefc5345
Richard Fenix <richard.fenix@gmail.com>
2018-02-21 06:26:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key) {
-  return delete Object.assign({}, object)[key];
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
